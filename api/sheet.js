4e7bfced89e71ebf06eea149cc148919e1a0f54c
dtinth on MBP M1 <dtinth@spacet.me>
2021-07-14 04:58:45 +0700

    Fix missing rquire

--- api/sheet.js
+++ api/sheet.js
@@ -3,7 +3,10 @@ if (
   process.env.SERVICE_ACCOUNT
 ) {
   // Write a service account key to the filesystem.
-  fs.writeFileSync('/tmp/service-account.json', process.env.SERVICE_ACCOUNT)
+  require('fs').writeFileSync(
+    '/tmp/service-account.json',
+    process.env.SERVICE_ACCOUNT,
+  )
   // Set the GOOGLE_APPLICATION_CREDENTIALS environment variable.
   process.env.GOOGLE_APPLICATION_CREDENTIALS = '/tmp/service-account.json'
 }
