73ff3ea35a89af14ecbd1d61aa0d93a1ca4e6506
dtinth on MBP M1 <dtinth@spacet.me>
2021-07-14 13:51:31 +0700

    Specify cache-control just for cloudflare because vercel strips some parts of it
    https://vercel.com/docs/edge-network/headers#:~:text=When%20s-maxage%20is%20set%2C%20the%20Network%20Layer%20strips%20the%20s-maxage%20directive%20and%20does%20not%20return%20it%20to%20the%20client.

--- api/sheet.js
+++ api/sheet.js
@@ -56,6 +56,12 @@ module.exports = async function (request, response) {
       'Cache-Control',
       `public, s-maxage=30, stale-while-revalidate=30, max-age=30`,
     )
+    // https://blogs.akamai.com/2021/06/targeted-cache-control.html
+    // https://blog.cloudflare.com/cdn-cache-control/
+    response.setHeader(
+      'Cloudflare-CDN-Cache-Control',
+      `public, s-maxage=30, stale-while-revalidate=30, max-age=30`,
+    )
     response.json({ values: data.values })
   } catch (e) {
     if (e.code === 403) {
